98a14eb1e51e14c6ac0379d991fcfd576cf8a137
Mar Helali <mhelali@apptio.com>
2022-06-08 15:00:21 -0700

    fix arguments for both namespace and ctx

--- reroll.sh
+++ reroll.sh
@@ -3,14 +3,28 @@
 # This script requires kubectl, kubectx, and kubens.
 # Use cluster name as optional argument. 
 # If argument is not passed, your current cluster will be used instead.
+# Use namespace as second optional argument. 
+# If second argument is not passed, the monitoring namespace will be used instead.
 
 if [ ! -z "$1" ]
   then
-    kubectx $1
+    ctx=$(kubectx $1 2>&1)
+    echo $ctx
+    if [[ $ctx == *"no context exists"* ]]; then
+      exit 1
+    fi
 fi
 
-
-kubens monitoring
+if [ ! -z "$2" ]
+  then
+    namespace=$(kubens $2 2>&1)
+    echo $namespace
+    if [[ $namespace == *"no namespace exists"* ]]; then
+      exit 1
+    fi
+else
+  kubens monitoring
+fi
 
 echo "Getting prometheus-operator pods..."
 kubectl get pods | grep prometheus-operator
